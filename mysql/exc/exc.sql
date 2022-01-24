6c11b751859d18815ddfe70eab13fa45d95f4fc2
dbstnqls12 <binson1123@naver.com>
2022-01-24 16:17:22 +0900

    asd

--- mysql/exc/exc.sql
+++ mysql/exc/exc.sql
@@ -18,3 +18,17 @@ create table member (
 );
 
 -- 테이블삭제
+
+drop table member2;
+drop table member;
+
+-- 테이블생성
+create table book (
+	seq int,
+	bookname varchar(100),
+    publisher varchar(100),
+    writer varchar(100),
+    price int
+);
+
+drop table book;
