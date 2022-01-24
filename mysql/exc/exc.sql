1797f3f30382d2119314a689eeef045328ffbf59
dbstnqls12 <binson1123@naver.com>
2022-01-24 11:51:58 +0900

    zxc

---
+++ mysql/exc/exc.sql
@@ -0,0 +1,20 @@
+use world;
+
+show tables;
+
+ -- 테이블 생성
+create table member2 (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+);
+
+create table member (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+);
+
+-- 테이블삭제
