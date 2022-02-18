c297cec56741668376b288f75926327aa7287bd4
dbstnqls12 <binson1123@naver.com>
2022-02-18 12:51:31 +0900

    asd

--- mysql/exc/monika_exc5.sql
+++ mysql/exc/monika_exc5.sql
@@ -122,4 +122,12 @@ from infrauthmenu a
 -- 	where a.ifatSeq =2;
 ;    
 
+use nct;
+WITH RECURSIVE CteCate(ifctSeq, ifctParents, ParentsName, ifctName, depth, orderString, ifctOrder, ifctDepth) AS 
+(
+    select ifctSeq, ifctParents, cast('' as char(30)), ifctName, 0, cast(ifctSeq as char(10)), ifctOrder, ifctDepth from infrCate where ifctParents is null
+    UNION ALL
+    select d.ifctSeq, d.ifctParents, cte.ifctName, d.ifctName, depth + 1, concat(cte.orderString, '-', d.ifctOrder), d.ifctOrder, d.ifctDepth from infrCate d inner join CteCate cte on d.ifctParents = cte.ifctSeq
+)
+select * from CteCate order by orderString;
 
