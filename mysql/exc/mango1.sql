3ee6df3c0a18ddfed9816014c686b235f3310f31
dbstnqls12 <binson1123@naver.com>
2022-02-25 10:41:51 +0900

    asd

--- mysql/exc/mango1.sql
+++ mysql/exc/mango1.sql
@@ -5,10 +5,12 @@ use kbook;
 select 
 	a.tditTitle
     ,a.tditSubTitle
-    
+   ,avg((select b.ifcdReferenceI2 from infrcode b where b.ifcgSeq=26 and b.ifcdOrder = c.tditKcloverCd)) as Kclover
+   ,count(tdirSeq) as reviewcount
+   
 		
 from tradItem a
-
+	left join traditemreview c on c.tditSeq = a.tditSeq
 where 1=1
-	and tditSeq=1
+	
 ;
