5ee5b45c10a8365a6e80b5f20357c88ab2473c83
dbstnqls12 <binson1123@naver.com>
2022-02-25 11:05:11 +0900

    asd

--- mysql/exc/mango1.sql
+++ mysql/exc/mango1.sql
@@ -4,13 +4,18 @@ use kbook;
 -- 책 정보 페이지 내용
 select 
 	a.tditTitle
-    ,a.tditSubTitle
-   ,avg((select b.ifcdReferenceI2 from infrcode b where b.ifcgSeq=26 and b.ifcdOrder = c.tditKcloverCd)) as Kclover
-   ,count(tdirSeq) as reviewcount
-   
-		
+	,a.tditSubTitle
+	,avg((select b.ifcdReferenceI2 from infrcode b where b.ifcgSeq=26 and b.ifcdOrder = c.tditKcloverCd)) as Kclover
+	,count(tdirSeq) as reviewcount
+	,(select e.ifacName from infrauthorcode e where e.ifacSeq = d.tdatAuthorCd) as tditAuthor
+	,(select f.ifpcName from infrpublishercode f where f.ifpcSeq = a.tditPublisherCd) as tditPublisher
+    ,a.tditPublishingDate
+	,a.tditPrice
+    ,(select b.ifcdName from infrcode b where b.ifcgSeq=13 and b.ifcdOrder = a.tditDiscountCd) as tditDisCount
+    
 from tradItem a
 	left join traditemreview c on c.tditSeq = a.tditSeq
+    left join tradauthor d on d.tditSeq = a.tditSeq
 where 1=1
 	
 ;
